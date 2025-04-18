3e14cf6fb60873dc403b6694860145f9dbd9e822
Adm. Jesus <134287070+jesusmjunior@users.noreply.github.com>
2025-04-17 23:35:29 -0300

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='MEMORIASJESUS')
+app = Flask(__name__)  # Usa a pasta padrão 'templates'
 
 @app.route("/")
 def home():
