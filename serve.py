d9ed381ce56262c069a6bc0b12190bb7f502b609
Adm. Jesus <134287070+jesusmjunior@users.noreply.github.com>
2025-04-18 00:03:38 -0300

    Update serve.py

--- serve.py
+++ serve.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='MEMORIASJESUS/templates')
+app = Flask(__name__)  # Usa a pasta padrão 'templates'
 
 @app.route("/")
 def home():
-    return render_template("memory.html")
+    return render_template("memory.html")  # Está em /templates
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
